16a2555bedf9af03dd175585f9f868fe63f0fc76
Tvard <i.tvard@gmail.com>
2023-08-09 16:28:57 +0300

    move validate rules to Request class

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,76 +2,88 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreArticleRequest;
+use App\Http\Requests\UpdateArticleRequest;
 use App\Models\Article;
 
 class ArticleController extends Controller
 {
-
-    public function destroy($id)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $article = Article::findOrFail($id);
-
-        if ($article) {
-            $article->delete();
-        }
-
-        return redirect()
-            ->route('articles.index')
-            ->with('success', 'article was delete');
+        $articles = Article::paginate();
+        $articles->links();
+        return view('article.index', compact('articles'));
     }
-    public function update(Request $request, $id)
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
     {
-        $article = Article::findOrFail($id);
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
 
-        $data = $this->validate($request, [
-            'name' => 'required|unique:articles,name,' . $article->id,
-            'body' => 'required|min:100'
-        ]);
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreArticleRequest $request)
+    {
+        $data = $request->validated();
 
+        $article = new Article(); 
         $article->fill($data);
         $article->save();
 
-        return redirect()->route('articles.index');
+        return redirect()
+           ->route('articles.index')
+           ->with('success, Article was create');
     }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $article = Article::find($id);
+        return view('article.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
     public function edit($id)
     {
         $article = Article::findOrFail($id);
         return view('article.edit', compact('article'));
     }
-    public function index()
-    {
-        $articles = Article::paginate(15);
-        $articles->links();
 
-        return view('article.index', compact('articles'));
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateArticleRequest $request, Article $obj)
+    {
+        $data = $request->validated();
+
+        $obj->fill($data);
+        $obj->save();
+
+        return redirect()->route('articles.index');
     }
 
-    public function show($id)
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
     {
-        $article = Article::findOrFail($id);
-        return view('article.show', compact('article'));
-    }
+        $data = Article::findOrFail($id);
 
-    public function create()
-    {
-        $article = new Article();
-
-        return view('article.create', compact('article'));
-    }
-
-    public function store(Request $request)
-    {
-        $data = $this->validate($request, [
-            'name' => "required|unique:articles",
-            'body' => "required|min:10"
-        ]);
-
-        $article = new Article();
-
-        $article->fill($data);
-
-        $article->save();
+        if ($data) {
+            $data->destroy($id);
+        }
 
         return redirect()->route('articles.index');
     }
--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get("about", [PageController::class, 'about']);
-
-Route::get('articles', [ArticleController::class, 'index'])
-    ->name('articles.index');
-Route::get('articles/create', 'ArticleController@create')
-    ->name('articles.create');
-Route::get('articles/{id}', [ArticleController::class, 'show'])
-    ->name('articles.show');
-Route::get('articles/{id}/edit', [ArticleController::class, 'edit'])
-    ->name('articles.edit');
-Route::post('articles', "ArticleController@store")
-    ->name('articles.store');
-Route::patch('articles/{id}', "ArticleController@update")
-    ->name("articles.update");
-Route::delete('articles/{id}', "ArticleController@destroy")
-    ->name('articles.destroy');
+Route::resource('articles', ArticleController::class);
